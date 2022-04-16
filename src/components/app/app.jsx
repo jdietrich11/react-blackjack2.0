078b1199a379b7bc8db6aab00dcdee0390063901
jdietrich11 <jbdietrich2@gmail.com>
2022-04-16 14:07:48 -0600

    switched app and header to functional component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -6,16 +6,14 @@ import Playfield from '../playfield/playfield';
 
 import './app.scss';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='app'>
-        <Overlay />
-        <Header />
-        <Playfield />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='app'>
+      <Overlay />
+      <Header />
+      <Playfield />
+    </div>
+  );
+};
 
 export default App;
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,27 +4,24 @@ import './header.scss';
 
 const logo = require('../../img/logo.png');
 
-class Header extends React.Component {
-  handleClick(e) {
-    e.preventDefault();
-    window.location.reload();
-  }
-  render() {
-    return (
-      <div className='header'>
-        <div className='header__logo'>
-          <img src={logo} alt='logo' className='logo' />
-        </div>
-        <div className='header__title'>React BlackJack</div>
-        <div
-          onClick={(e) => this.handleClick(e)}
-          className='header__new-game btn'
-        >
-          New Game
-        </div>
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header__logo'>
+        <img src={logo} alt='logo' className='logo' />
       </div>
-    );
-  }
-}
+      <div className='header__title'>React BlackJack</div>
+      <div
+        onClick={(e) => {
+          e.preventDefault();
+          window.location.reload();
+        }}
+        className='header__new-game btn'
+      >
+        New Game
+      </div>
+    </div>
+  );
+};
 
 export default Header;
